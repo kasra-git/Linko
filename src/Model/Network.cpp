3e7bfc9c4f2efe238ee025ddca563c064dac0589
Kasra <kasraalirezaie25@gmail.com>
2026-01-27 13:05:23 +0330

    feat<Network.cpp> : add login_user() for Network class

--- src/Model/Network.cpp
+++ src/Model/Network.cpp
@@ -12,10 +12,22 @@ void Network::register_user(
     
 }
 
-// check-password-for-user
+// check-password-for-user to use in login_user() method
 bool Network::check_password(
     User& user,
     std::string password
 ){
     return user.show_hashed_password() == password;
+}
+
+// login-user
+User* Network::login_user(
+    std::string user_name,
+    std::string password
+){
+    for(auto& user : users){
+        if (user.show_user_name() == user_name && check_password(user,password))  
+            return &user;
+    }
+    return nullptr;
 }
