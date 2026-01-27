bce9c3b5289ce3c9a793bc1e166e1c2119046f26
Kasra <kasraalirezaie25@gmail.com>
2026-01-27 12:58:48 +0330

    feat<Network.hpp> : add simple version of Network class

---
+++ include/Model/Network.hpp
@@ -0,0 +1,20 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+#include <vector>
+#include "./User.hpp"
+
+class Network{
+    public:
+        Network() = default;
+
+        // Register-New-User
+        void register_user(std::string user_name , std::string password , std::string display_name , std::string bio);
+        bool check_password(User& user,std::string password);
+        // Login-User
+        User* login_user(std::string display_name , std::string password);
+    private: 
+        // Vector of users   
+        std::vector<User> users;
+};
+#endif
