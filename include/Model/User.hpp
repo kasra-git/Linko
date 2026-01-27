65a9b53fcd6eb6823c29e02c02ddc25c6a103a62
Kasra <kasraalirezaie25@gmail.com>
2026-01-27 12:48:39 +0330

    feat<User.hpp> : add simple version of User class

---
+++ include/Model/User.hpp
@@ -0,0 +1,18 @@
+#ifndef USER_HPP
+#define USER_HPP
+
+#include <string>
+
+class User{
+    public:
+        User(std::string con_u , std::string con_p , std::string con_d , std::string con_b);
+        std::string& show_user_name();
+        std::string& show_hashed_password();
+    private:
+        std::string userName;
+        std::string password;
+        std::string displayName;
+        std::string bio;
+};
+
+#endif
