3b3ea2ebb28ea16a4c16033b0f3cddb8d154040f
Kasra <kasraalirezaie25@gmail.com>
2026-02-08 19:07:49 +0330

    feat: add basic Network class to Network.hpp

---
+++ include/Network.hpp
@@ -0,0 +1,14 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+#include <vector>
+#include "User.hpp"
+
+class User;
+class Network{
+    public:
+    private:    
+        std::vector<User> users;
+
+};
+
+#endif
