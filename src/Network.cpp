f9b1e9546a5a5511fdb9d3254726d4825466eb8d
Kasra <kasraalirezaie25@gmail.com>
2026-02-08 19:24:37 +0330

    feat: add add_user() functionality

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,1 +1,11 @@
 #include "../include/Network.hpp"
+#include <string>
+
+void Network::add_user(
+    std::string new_userName,
+    std::string new_password,
+    std::string new_displayName,
+    std::string new_bio
+){
+    users.push_back(User(new_userName,new_password,new_displayName,new_bio));
+}
