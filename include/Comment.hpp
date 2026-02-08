e3c808c60ef1b7b0073d82a8bd735013900e9ec9
Kasra <kasraalirezaie25@gmail.com>
2026-02-08 19:54:41 +0330

    feat : add simple Comment class to Comment.hpp

---
+++ include/Comment.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMENT_HPP
+#define COMMENT_HPP
+#include <string>
+
+class Comment{
+    public: 
+        Comment();
+    private:
+        std::string author;
+        std::string text;
+};
+#endif
