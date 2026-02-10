d292d3ece99a4ef9c3af3b03a59ca31c7db4190c
Kasra <kasraalirezaie25@gmail.com>
2026-02-10 10:36:53 +0330

    feat: add new functions for better functionality

--- include/User.hpp
+++ include/User.hpp
@@ -1,15 +1,21 @@
 #ifndef USER_HPP
 #define USER_HPP
 #include <string>
+#include <vector>
 
 class User{
     public:
-        User(std::string,std::string,std::string,std::string);
+        User(const std::string&,const std::string&,const std::string&,const std::string&);
+        std::string getUsername() const;
+        bool checkPassword(const std::string& password) const;
+        const std::vector<std::string>& getFollwing()const;
+
     private:
         std::string userName;
         std::string password;
         std::string displayName;
-        std::string bio;      
+        std::string bio;   
+        std::vector<std::string> following;  
 };
 
 
