a4699d2c5bc20cb87223e4048e0a749134763e35
Kasra <kasraalirezaie25@gmail.com>
2026-02-10 12:58:38 +0330

    feat: write registerUser() functionality

--- src/AuthManager.cpp
+++ src/AuthManager.cpp
@@ -1,1 +1,12 @@
 #include "../include/AuthManager.hpp"
+
+AuthManager::AuthManager(Network& n) : network(n){}
+
+bool AuthManager::registerUser(
+    const std::string& u,
+    const std::string& p,
+    const std::string& d,
+    const std::string& b
+){return network.addUser(User(u,p,d,b));}
+
+
