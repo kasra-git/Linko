96eb2abb916e6d790982fa887e8a2d2aa4c3347d
Kasra <kasraalirezaie25@gmail.com>
2026-02-10 10:44:50 +0330

    feat: write new functionalities of User class

--- src/User.cpp
+++ src/User.cpp
@@ -1,8 +1,13 @@
 #include "../include/User.hpp"
 
 User::User(
-    std::string co_userName,
-    std::string co_password,
-    std::string co_displayName,
-    std::string co_bio
+    const std::string& co_userName,
+    const std::string& co_password,
+    const std::string& co_displayName,
+    const std::string& co_bio
 ): userName(co_userName) , password(co_password) , displayName(co_displayName) , bio(co_bio){}
+
+std::string User::getUsername()const {return this->userName;}
+bool User::checkPassword(const std::string& password)const{return password == this->password;}
+void User::follow(const std::string& user){this->following.push_back(user);}
+const std::vector<std::string>& User::getFollwing() const {return this->following;}
