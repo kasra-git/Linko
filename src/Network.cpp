3b07d9245fd08ec732dc6d0024aa36a64e818a65
Kasra <kasraalirezaie25@gmail.com>
2026-02-10 12:34:01 +0330

    feat: write addUser() functionality

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,1 +1,8 @@
 #include "../include/Network.hpp"
+
+bool Network::addUser(const User& new_user){
+    if (users.count(new_user.getUsername()))
+        return false;
+    users[new_user.getUsername()] = new_user;
+    return true;
+}
