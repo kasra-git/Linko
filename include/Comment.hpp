5c6325e409f5291d977c2d3f705694bfedb242a3
Kasra <kasraalirezaie25@gmail.com>
2026-02-19 17:06:48 +0330

    fix

--- include/Comment.hpp
+++ include/Comment.hpp
@@ -1,7 +1,6 @@
 #ifndef COMMENT_HPP
 #define COMMENT_HPP
 #include <string>
-#include <ctime>
 
 class Comment{
     public: 
@@ -9,7 +8,6 @@ class Comment{
 
         std::string getAuthor() const;
         std::string getText() const;
-        std::time_t getTime() const;
     private:
         std::string author;
         std::string text;
--- src/Comment.cpp
+++ src/Comment.cpp
@@ -3,8 +3,7 @@
 Comment::Comment(
     const std::string& author,
     const std::string& text
-):author(author),text(text){this->timeStamp = std::time(nullptr);}
+):author(author),text(text){}
 
 std::string Comment::getAuthor() const {return this->author;}
 std::string Comment::getText() const {return this->text;}
-std::time_t Comment::getTime() const {return this->timeStamp;}
